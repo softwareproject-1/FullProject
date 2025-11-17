7b1e256bf9b6fd3c457286c152d324f07778aa39
OmaroHh <omar.abdalla@student.giu-uni.de>
2025-11-17 13:52:53 +0200

    edit

--- Employee-Organization-Performance/Backend/src/app.module.ts
+++ Employee-Organization-Performance/Backend/src/app.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Employee, EmployeeSchema } from '../Models/employeeSchema';
+import {
+  Department,
+  DepartmentSchema,
+} from '../Models/Organization Schema/departmenSchema';
+import {
+  Position,
+  PositionSchema,
+} from '../Models/Organization Schema/postionSchema';
+import { User, UserSchema } from '../Models/userSchema';
+import {
+  AppraisalTemplate,
+  AppraisalTemplateSchema,
+} from '../Models/performance/AppraisalTemplate';
+import {
+  AppraisalCycle,
+  AppraisalCycleSchema,
+} from '../Models/performance/AppraisalCycle';
+import {
+  PerformanceAppraisal,
+  PerformanceAppraisalSchema,
+} from '../Models/performance/PerformanceAppraisal';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://team1:notPassword@cluster0.4mleald.mongodb.net/?appName=Cluster0',
+    ),
+    MongooseModule.forFeature([
+      { name: Employee.name, schema: EmployeeSchema },
+      { name: Department.name, schema: DepartmentSchema },
+      { name: Position.name, schema: PositionSchema },
+      { name: User.name, schema: UserSchema },
+      { name: AppraisalTemplate.name, schema: AppraisalTemplateSchema },
+      { name: AppraisalCycle.name, schema: AppraisalCycleSchema },
+      { name: PerformanceAppraisal.name, schema: PerformanceAppraisalSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
